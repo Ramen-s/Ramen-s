08a5954520e2042440ad38e4f6886ee75147b1e7
Ubuntu <ubuntu@ip-172-31-20-231.ap-northeast-1.compute.internal>
2026-02-19 07:59:54 +0000

    顧客マイページ編集画面、ログイン機能作成

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,8 @@
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
-ActiveRecord::Schema[8.0].define(version: 2026_02_18_050704) do
+
+ActiveRecord::Schema[8.0].define(version: 2026_02_19_070308) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -76,6 +77,12 @@ ActiveRecord::Schema[8.0].define(version: 2026_02_18_050704) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+    t.string "last_name"
+    t.string "first_name"
+    t.string "postal_code"
+    t.string "address"
+    t.string "kana"
+    t.string "telephone_number"
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
@@ -96,6 +103,29 @@ ActiveRecord::Schema[8.0].define(version: 2026_02_18_050704) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "order_details", force: :cascade do |t|
+    t.integer "order_id"
+    t.integer "item_id"
+    t.integer "price"
+    t.integer "amount"
+    t.integer "making_status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "customer_id", null: false
+    t.string "postal_code", null: false
+    t.string "address", null: false
+    t.string "name", null: false
+    t.integer "shipping_cost", null: false
+    t.integer "total_payment", null: false
+    t.integer "payment_method", default: 0, null: false
+    t.integer "status", default: 0, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -108,8 +138,6 @@ ActiveRecord::Schema[8.0].define(version: 2026_02_18_050704) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  
 end
